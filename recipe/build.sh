0b57a234ca4a8d24026b2f60d695f2bae3f736d3
Chris Burr <chrisburr@users.noreply.github.com>
2022-01-23 18:28:40 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ cd build
 cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DLIB_SUFFIX="" \
+    -DGSOAP_WSDL2H=$BUILD_PREFIX/bin/wsdl2h \
+    -DGSOAP_SOAPCPP2=$BUILD_PREFIX/bin/soapcpp2 \
     ..
 
 make -j${CPU_COUNT}
